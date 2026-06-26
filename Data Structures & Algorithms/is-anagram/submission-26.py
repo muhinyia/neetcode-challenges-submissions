d93ce39a3888f99ce33bc63a07700a2fda17a408
Muhinyia wa Ndegwa <36927035+muhinyia@users.noreply.github.com>
2026-06-26 19:03:04 +0100

    Add: is-anagram - submission-26

---
+++ Data Structures & Algorithms/is-anagram/submission-26.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashMap = {}
+        if len(s) != len(t):
+            return False
+        for char in s:
+            if char in hashMap:
+                hashMap[char] += 1
+            else:
+                hashMap[char] = 1
+
+        for char in t:
+            if char not in hashMap:
+                return False
+            else:
+                if hashMap[char] == 1:
+                    hashMap.pop(char)
+                else:
+                    hashMap[char] -= 1
+        return len(hashMap)==0
