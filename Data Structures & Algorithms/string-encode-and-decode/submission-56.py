b2d1560df20ea3bbac5598074c2b33a204ec0db1
Muhinyia wa Ndegwa <36927035+muhinyia@users.noreply.github.com>
2026-09-09 13:07:35 +0100

    Add: string-encode-and-decode - submission-56

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-56.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return "????"
+        encodedString = ""
+        for i in range(len(strs)):
+
+            encodedString += strs[i]
+            if i <len(strs)-1:
+                encodedString += "*#£SPLITHERE*#£"
+        return encodedString
+
+    def decode(self, s: str) -> List[str]:
+        if s =="????":
+            return []
+        strs = []
+        for st in s.split("*#£SPLITHERE*#£"):
+            strs.append(st)
+        return strs
